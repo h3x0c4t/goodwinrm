f50edfc9170569dc1084d58674a98e96be7715c8
h3x0c4t <118213553+h3x0c4t@users.noreply.github.com>
2024-05-18 19:41:47 +0300

    fix brackets

--- goodwinrm/goodwinrm.py
+++ goodwinrm/goodwinrm.py
@@ -6,38 +6,38 @@ from winrm.protocol import Protocol
 
 # Print the banner
 def PrintBanner():
-    banner = HTML('''
+    banner = HTML("""
 > <ansigreen>┏┓     ┓┓ ┏•  ┳┓ ┳┳┓</ansigreen>  <b>GoodWinRM - v0.1</b>
 > <ansigreen>┃┓┏┓┏┓┏┫┃┃┃┓┏┓┣┻┓┃┃┃</ansigreen>  Python WinRM Remote Shell
 > <ansigreen>┗┛┗┛┗┛┗┻┗┻┛┗┛┗┛ ┗┛ ┗</ansigreen>  <ansiblue>https://github.com/h3x0c4t/goodwinrm</ansiblue>
-    ''')
+    """)
 
     printf(banner)
 
 # Parse the command line arguments
 def ParseArguments():
-    parser = argparse.ArgumentParser(description='WinRM Remote Shell')
-    parser.add_argument('-i', '--ip', help='Remote host IP or hostname.', required=True)
-    parser.add_argument('-u', '--username', help='Username.', required=True)
-    parser.add_argument('-p', '--password', help='Password.', required=True)
-    parser.add_argument('-t', '--transport', help="Transport protocol. ['basic', 'ntlm', 'kerberos', 'credssp', 'ssl', 'certificate']", default='basic')
-    parser.add_argument('-v', '--server_cert_validation', help="Server certificate validation. ['ignore', 'validate']", default='ignore')
-    parser.add_argument('-d', '--directory', help='Working directory.', default='C:\\')
-    parser.add_argument('--https', help='Use HTTPS.', type=bool, default=False)
+    parser = argparse.ArgumentParser(description="WinRM Remote Shell")
+    parser.add_argument("-i", "--ip", help="Remote host IP or hostname.", required=True)
+    parser.add_argument("-u", "--username", help="Username.", required=True)
+    parser.add_argument("-p", "--password", help="Password.", required=True)
+    parser.add_argument("-t", "--transport", help="Transport protocol. ['basic', 'ntlm', 'kerberos', 'credssp', 'ssl', 'certificate']", default="basic")
+    parser.add_argument("-v", "--server_cert_validation", help="Server certificate validation. ['ignore', 'validate']", default="ignore")
+    parser.add_argument("-d", "--directory", help="Working directory.", default="C:\\")
+    parser.add_argument("--https", help="Use HTTPS.", type=bool, default=False)
     args = parser.parse_args()
     return args
 
 def PrintError(message):
-    printf(ANSI('\x1b[31m[!] {0}\x1b[0m').format(message))
+    printf(ANSI("\x1b[31m[!] {0}\x1b[0m").format(message))
 
 def PrintSuccess(message):
-    printf(ANSI('\x1b[32m[+] {0}\x1b[0m').format(message))
+    printf(ANSI("\x1b[32m[+] {0}\x1b[0m").format(message))
 
 # Open a remote shell
-# server_cert_validation values: 'ignore', 'validate'
-# transport values: 'basic', 'ntlm', 'kerberos', 'credssp', 'ssl', 'certificate'
-def OpenRemoteShell(address, username, password, https=False, transport='basic', server_cert_validation='ignore', directory='C:\\'):
-    address = address.strip().split(':')
+# server_cert_validation values: "ignore", "validate"
+# transport values: "basic", "ntlm", "kerberos", "credssp", "ssl", "certificate"
+def OpenRemoteShell(address, username, password, https=False, transport="basic", server_cert_validation="ignore", directory="C:\\"):
+    address = address.strip().split(":")
     if len(address) == 2:
         address, port = address
     else:
@@ -47,11 +47,11 @@ def OpenRemoteShell(address, username, password, https=False, transport='basic',
     if https:
         if port == 0:
             port = 5986
-        endpoint = f'https://{address}:{port}/wsman'
+        endpoint = f"https://{address}:{port}/wsman"
     else:
         if port == 0:
             port = 5985
-        endpoint = f'http://{address}:{port}/wsman'
+        endpoint = f"http://{address}:{port}/wsman"
 
     p = Protocol(
         endpoint=endpoint,
@@ -62,27 +62,27 @@ def OpenRemoteShell(address, username, password, https=False, transport='basic',
     )
     try:
         shell_id = p.open_shell(codepage=65001, working_directory=directory)
-        PrintSuccess(f'Successfully connected to {address}.')
+        PrintSuccess(f"Successfully connected to {address}.")
         print()
     except Exception as e:
-        PrintError(f'Fatal: {e}')
+        PrintError(f"Fatal: {e}")
         exit(1)
     return p, shell_id
     
 def ExecuteCommand(cmd, p, shell_id):
-    encyptedCmd = b64encode(cmd.encode('utf_16_le')).decode('ascii')
-    command_id = p.run_command(shell_id, f'powershell -enc {encyptedCmd}')
+    encyptedCmd = b64encode(cmd.encode("utf_16_le")).decode("ascii")
+    command_id = p.run_command(shell_id, f"powershell -enc {encyptedCmd}")
     std_out, std_err, status_code = p.get_command_output(shell_id, command_id)
     p.cleanup_command(shell_id, command_id)
 
     if status_code != 0:
         if std_out:
-            PrintError(std_out.decode('utf-8', 'replace').strip())
+            PrintError(std_out.decode("utf-8", "replace").strip())
         if std_err:
-            PrintError(f'Error: {std_err.decode('utf-8', 'replace').strip()}')
+            PrintError(f"Error: {std_err.decode('utf-8', 'replace').strip()}")
         return
 
-    output = std_out.decode('utf-8', 'replace')
+    output = std_out.decode("utf-8", "replace")
     print(output.strip())
 
 def main():
@@ -100,16 +100,16 @@ def main():
     )
 
     session = PromptSession()
-    completer = WordCompleter(['exit', 'clear'])
+    completer = WordCompleter(["exit", "clear"])
     while(True):
-        cmd = session.prompt(ANSI(f'\x1b[34m{args.username}@{args.ip}\x1b[0m > '), completer=completer)
-        if cmd == 'exit':
+        cmd = session.prompt(ANSI(f"\x1b[34m{args.username}@{args.ip}\x1b[0m > "), completer=completer)
+        if cmd == "exit":
             break
-        if cmd == 'clear':
+        if cmd == "clear":
             # Only works in Linux
-            print('\033[H\033[J')
+            print("\033[H\033[J")
             continue
-        if cmd == '':
+        if cmd == "":
             continue
 
         ExecuteCommand(cmd, p, shell_id)
@@ -117,5 +117,5 @@ def main():
 
     p.close_shell(shell_id)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ import os
 def read(fname):
     return open(fname).read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
     name = "GoodWinRM",
     version = "0.1",
     description="Python WinRM Remote Shell",
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     url = "https://github.com/h3x0c4t/goodwinrm",
     author = "nu11z",
     packages=find_packages(),
     install_requires=requirements,
     entry_points = {
-        'console_scripts': ['goodwinrm=goodwinrm.goodwinrm:main'],
+        "console_scripts": ["goodwinrm=goodwinrm.goodwinrm:main"],
     }
 )
